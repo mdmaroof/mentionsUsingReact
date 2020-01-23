0e14feae79aa75b70e1180dcab796f2eaf46acd8
maroof <maroof@noonacademy.com>
2020-01-23 18:33:26 +0530

    find value and hyperlinking mentions

--- src/app.js
+++ src/app.js
@@ -13,13 +13,22 @@ class App extends Component {
         isLoad:false,
         inputValue:'',
         filterValue:'',
-        array:[]
+        array:[],
+        saveArray:[],
+
+        showProfile:false,
+        profileData:{},
     }
 
     clickedData = (value) => {
-        const data = [...this.state.array.slice(0,this.state.array.length-1),`@${value}`];
-        const changedInput = data.join(' ')
+        // const valueData = <a href="#">@{value}</a>;
+        const valueData = {type:'href',value:`@${value}`}
+        // console.log('--->',valueData)
+        const data = [...this.state.array.slice(0,this.state.array.length-1),valueData.value];
+        const changedInput = data.join(' ');
+
         this.setState({
+            saveArray:[...this.state.saveArray,valueData],
             array:data,
             inputValue:changedInput,
             isLoad:false,
@@ -53,37 +62,75 @@ class App extends Component {
         })
     }
 
+    showProfile = (data) => {
+        const slicedValue = data.slice(1);
+        const searchedResult = jsonData.find(o => o.name === slicedValue)
+        this.setState({
+            showProfile:true,
+            profileData:searchedResult,
+        })
+    }
     render() {
-   
+      const filterValue = jsonData.filter(z => z.name.includes(this.state.filterValue));
         return(
 
             <div className="container">
                 <h1>
                     Input search List
                 </h1>
-                <input value={this.state.inputValue} onChange={this.typeValue} style={{width:'400px',fontSize:'20px',marginTop:'40px',marginLeft:'10px'}}/>
-                {this.state.isLoad ?
-                    <div style={{background:'#000',color:'#fff',padding:'20px 10px',position:'absolute',width:'300px',left:'20px'}}>
-                        {jsonData.filter(z=>z.name.includes(this.state.filterValue)).map(x =>{
-                            return(
-                                <li key={x.id} onClick={ () =>this.clickedData(x.name) }> {x.name} </li>
-                            )
-                        })}
-                    </div>
-                    :
-                    ''
-                }
-                <br/>
 
-                {
-                    this.state.array.map((x,i)=>{
+                <input value={this.state.inputValue} onChange={this.typeValue} style={{width:'400px',fontSize:'20px',marginTop:'40px',marginLeft:'10px'}}/>
+                    
+                    {this.state.isLoad ?
+                        <div style={{background:'#000',color:'#fff',padding:'20px 10px',position:'absolute',width:'300px'}}>
+                            {filterValue.length === 0 ?
+                                'No Result'
+                            :
+                                filterValue.map((x,i) =>{
+                                    return(
+                                        <li key={x.id} onClick={ () =>this.clickedData(x.name) }> {x.name} </li>
+                                    )
+                                })
+                            }
+                        </div>
+                        :
+                        ''
+                    }
+                <br/> <br/> <br/>
+
+                {this.state.inputValue.split(" ").map((x,i)=>{
+                    const data = this.state.saveArray.find(o => o.value === x) || false;
+                    if(data === false){
                         return(
-                        <React.Fragment key={i}>
-                            &nbsp;{x}&nbsp;&nbsp;
-                        </React.Fragment>
+                            <React.Fragment key={i}>{x} &nbsp;</React.Fragment>
                         )
-                    })
+                    }
+                    else{
+                        return(
+                            <React.Fragment key={i}>
+                                <span onClick={()=>this.showProfile(x)} style={{padding:'2px 4px',background:'#000',color:'#ff0000'}} href="#">{x}</span>  &nbsp;
+                            </React.Fragment>
+                            
+                        )
+                    }
+                    
                 }
+                ) || 'Type Something'}
+
+
+                <hr />
+                <br />
+
+                {this.state.showProfile ?
+                 <>
+                    <b>Profile ID : </b> {this.state.profileData.id}<br/>
+                    <b>Profile Name : </b> {this.state.profileData.name}
+                 </>
+                 :
+                 ''
+                }
+
+
             </div>
         )
     }
